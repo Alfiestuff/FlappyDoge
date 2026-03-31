34494f8b0c821fa6945c37fc7c623513e3f2b73d
Alfiestuff <yepperyeppersone@gmail.com>
2026-03-31 17:19:12 +0100

    hotfix

--- classes/__int__.py
+++ classes/__int__.py
@@ -1,11 +1,47 @@
-img = pygame.image.load("Images/Doge.png").convert_alpha()
+import pygame
 
-size = 70  # width you want
-ratio = img.get_height() / img.get_width()
 
-self.doge = pygame.transform.scale(img, (size, int(size * ratio)))
+class DogeUI:
+    def __init__(self, screen, size=120):
+        self.screen = screen
+        self.W, self.H = screen.get_size()
 
-self.lang_rect = pygame.Rect(0, 0, 260, 30)
-self.lang_rect.topleft = (20, 105)
+        # load image safely
+        img = pygame.image.load("Images/Doge.png").convert_alpha()
 
-self.doge_pos = (W // 2 + 200, H // 2 - 50)
+        ratio = img.get_width() / img.get_height()
+        self.doge = pygame.transform.scale(img, (size, int(size / ratio)))
+
+        # position doge
+        self.doge_pos = (self.W // 2 + 200, self.H // 2 - 50)
+
+        # language system (simple example)
+        self.languages = ["EN", "ES", "FR", "DE"]
+        self.lang_index = 0
+
+        self.font = pygame.font.SysFont("Courier", 24)
+
+        # language button
+        self.lang_rect = pygame.Rect(20, 105, 260, 30)
+
+    def toggle_language(self):
+        self.lang_index = (self.lang_index + 1) % len(self.languages)
+
+    def draw(self):
+        # draw doge
+        self.screen.blit(self.doge, self.doge_pos)
+
+        # draw language button
+        lang_text = self.font.render(
+            f"Language: {self.languages[self.lang_index]}",
+            True,
+            (255, 255, 255)
+        )
+
+        pygame.draw.rect(self.screen, (60, 60, 60), self.lang_rect, border_radius=6)
+        self.screen.blit(lang_text, (self.lang_rect.x + 10, self.lang_rect.y + 5))
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.lang_rect.collidepoint(event.pos):
+                self.toggle_language()
